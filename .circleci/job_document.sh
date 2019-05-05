fcdd970531e19b283e9eac78ac1999eea54e2137
Fabian Sorn <fabian.sorn@online.de>
2019-05-05 19:58:32 +0200

    Deactivate Latex Job to save build minutes

--- .circleci/job_document.sh
+++ .circleci/job_document.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 cd documentation/Latex/
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "|            Create output directory           |"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 rm -rf documentation_output 
 mkdir documentation_output
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "|        Generate PDF from Latex file          |"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 latexmk -pdf -jobname=documentation_output/Ausarbeitung Ausarbeitung.tex
-# Move output to Artifacts Folder
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "|    Copy generated file to build artifacts    |"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 mkdir /tmp/artifacts 
 cp -R documentation_pdf/*.pdf /tmp/artifacts/
