3e66791dff2bc404346ffd7825ccee48aaef4960
Fabian Sorn <fabian.sorn@online.de>
2019-05-09 20:39:12 +0200

    Fix für unseren Bug

--- example_project/passwordmanager_user/PasswordManagerUser.py
+++ example_project/passwordmanager_user/PasswordManagerUser.py
@@ -11,6 +11,6 @@ class PasswordManagerUser():
     def hash_password(self, pw_from_manager: str) -> str:
         if not isinstance(pw_from_manager, str):
             raise ValueError("Password is not a string")
-        if not len(pw_from_manager) == 16:
+        if not len(pw_from_manager) > 0:
             raise ValueError("Password is too short")
         return (hashlib.sha256(('%s' % (pw_from_manager)).encode('utf-8')).hexdigest())
